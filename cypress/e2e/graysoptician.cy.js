4490e258519f99173d0613c86c6e18a796c184a2
aqib-prac <salman670ali@gmail.com>
2024-01-22 14:47:55 +0500

    new update

--- cypress/e2e/graysoptician.cy.js
+++ cypress/e2e/graysoptician.cy.js
@@ -28,7 +28,7 @@ describe("Testing form ", ()=>{
   
       cy.get('#email').type(validData.email);
   
-      cy.get('#message').type(validData.message)hfjfhj;
+      cy.get('#message').type(validData.message);
   
   
   
